b0c2d70c426b158bb08286434e57832ce93689e7
debnil <debnilsarkar72>
2026-05-01 13:20:19 +0000

    first commit

--- main.tf
+++ main.tf
@@ -21,10 +21,11 @@ provider "aws" {
 
 module "vpc" {
   source = "./modules/vpc"
-  vpc_cidr = var.vpc_cidr
+  vpc_cidr_block = var.vpc_cidr
   public_subnet_cidrs = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   cluster_name = var.cluster_name
+  availability_zones = var.availability_zones
 }
 
 module "eks" {
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,21 +1,32 @@
 variable "cluster_name" {
-    description = "EKS Cluster Name"
-    type = string
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
 }
 
 variable "subnet_ids" {
-    description = "List of subnet IDs for the EKS cluster"
-    type = list(string)
+  description = "Subnet IDs"
+  type        = list(string)
 }
+
 variable "node_groups" {
-  description = "EKS Node group configuration"
-  type=map(object({
+  description = "EKS node group configuration"
+  type = map(object({
     instance_types = list(string)
-    capacity_type = string
+    capacity_type  = string
     scaling_config = object({
       desired_size = number
-      max_size = number
-      min_size = number
+      max_size     = number
+      min_size     = number
     })
   }))
 }
